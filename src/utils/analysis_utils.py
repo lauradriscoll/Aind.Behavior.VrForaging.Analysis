0986bb412408ec0e3b1beba8738f01158ede6c2b
tiffanyona <tiffany.ona@alleninstitute.org>
2024-02-05 17:08:24 -0800

    Added a simple function to remove consecutive non-stops

--- src/utils/analysis_utils.py
+++ src/utils/analysis_utils.py
@@ -1,4 +1,7 @@
 from scipy.signal import lfilter, firwin
+import pandas as pd
+
+
 
 def fir_filter(data, cutoff_hz, num_taps=61, nyq_rate=1000/2.):
 
@@ -17,3 +20,31 @@ def fir_filter(data, cutoff_hz, num_taps=61, nyq_rate=1000/2.):
     data["filtered_velocity"] = lfilter(fir_coeff, 1.0, data["velocity"].values)
     
     return data
+
+def choose_cut(reward_sites: pd.DataFrame, number_skipped: int = 20):
+    '''
+    Choose the cut of the session based on the number of skipped sites
+    
+    Inputs:
+    reward_sites: pd.DataFrame
+        Dataframe with the reward sites
+    number_skipped: int
+        Number of skipped sites to choose the cut
+        
+    Returns:
+    int
+        The cut value of the session
+        
+    '''
+    
+    cumulative = 0
+    for row,i in enumerate(reward_sites.has_choice):
+        if int(i) == 0:
+            cumulative += 1
+        else:
+            cumulative = 0
+        
+        if cumulative == number_skipped:
+            return reward_sites.iloc[row].active_patch
+        
+    return max(reward_sites.active_patch)
