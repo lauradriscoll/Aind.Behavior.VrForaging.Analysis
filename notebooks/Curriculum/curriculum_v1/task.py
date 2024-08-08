e10eff1cbf94a2487f17594a58f25cb8d958408a
tiffany.ona <tiffany.ona@gmail.com>
2024-08-07 17:02:45 -0700

    upgrade task script with more info

--- notebooks/Curriculum/curriculum_v1/task.py
+++ notebooks/Curriculum/curriculum_v1/task.py
@@ -7,9 +7,67 @@ from typing import Literal
 
 from pydantic import Field, ValidationInfo, field_validator
 
-from aind_behavior_curriculum import Task, TaskParameters
+import aind_behavior_vr_foraging.task_logic as vr_task_logic
+import distribution_helpers as dist
+
+class updaters:
+    """
+    Updaters
+    """
+    pass
 
 
+vr_task_logic.AindVrForagingTaskLogic(
+    name= 'test',
+    )
+
+operation_control = vr_task_logic.OperationControl(
+    movable_spout_control=vr_task_logic.MovableSpoutControl(
+        enabled=False,
+        time_to_collect_after_reward=1,
+        retracting_distance=2000,
+    ),
+    audio_control=vr_task_logic.AudioControl(duration=0.2, frequency=5000),
+    odor_control=vr_task_logic.OdorControl(
+        valve_max_open_time=10, target_odor_flow=100, target_total_flow=1000, use_channel_3_as_carrier=True
+    ),
+    position_control=vr_task_logic.PositionControl(
+        gain=vr_task_logic.Vector3(x=1, y=1, z=1),
+        initial_position=vr_task_logic.Vector3(x=0, y=2.56, z=0),
+        frequency_filter_cutoff=5,
+        velocity_threshold=8,
+    ),
+)
+
+patch1 = vr_task_logic.PatchStatistics(
+    label="Amyl Acetate",
+    state_index=0,
+    odor_specification=vr_task_logic.OdorSpecification(index=0, concentration=1),
+    reward_specification=dist.CountUntilDepleted(available_water= 1000, amount_drop=5),
+    virtual_site_generation=vr_task_logic.VirtualSiteGeneration(
+        inter_patch=dist.InterPatch_VirtualSiteGeneratorHelper(),
+        inter_site=dist.InterSite_VirtualSiteGeneratorHelper(),
+        reward_site=dist.Reward_VirtualSiteGeneratorHelper(),
+    ),
+)
+
+environment_statistics = vr_task_logic.EnvironmentStatistics(
+    first_state=0, transition_matrix=vr_task_logic.Matrix2D(data=[[1]]), patches=[patch1]
+)
+    
+
+task_logic=vr_task_logic.AindVrForagingTaskLogic(
+    task_parameters=vr_task_logic.AindVrForagingTaskParameters(
+        rng_seed=None,
+        environment_statistics=environment_statistics,
+        task_mode_settings=vr_task_logic.ForagingSettings(),
+        operation_control=operation_control,
+    )
+)
+
+with open(f'local/vr_task_preward_intercept_stage{stage}.json', "w") as f:
+    f.write(task_logic.model_dump_json(indent=3))
+    
 class ExampleTaskParameters(TaskParameters):
     """
     Example Task Parameters
@@ -23,12 +81,12 @@ class ExampleTaskParameters(TaskParameters):
     field_4: float = Field(default=0.5, ge=0.0, le=1.0)
     field_5: Literal["Immutable Field"] = "Immutable Field"
 
-    # Optional: Add additional validation to fields.
-    @field_validator("field_1", "field_2")
-    @classmethod
-    def check_something(cls, v: int, info: ValidationInfo):
-        """Your validation code here"""
-        return v
+    # # Optional: Add additional validation to fields.
+    # @field_validator("field_1", "field_2")
+    # @classmethod
+    # def check_something(cls, v: int, info: ValidationInfo):
+    #     """Your validation code here"""
+    #     return v
 
 
 class ExampleTask(Task):
