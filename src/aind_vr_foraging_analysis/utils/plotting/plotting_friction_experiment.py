244b3fc5c5ad76fb97a0b980afbb8a74b6203592
tiffany.ona <tiffany.ona@gmail.com>
2025-08-07 18:00:26 -0700

    add documentation to friction functions

--- src/aind_vr_foraging_analysis/utils/plotting/plotting_friction_experiment.py
+++ src/aind_vr_foraging_analysis/utils/plotting/plotting_friction_experiment.py
@@ -1,8 +1,6 @@
 import sys
 sys.path.append('../../../src/')
 
-
-
 # Plotting libraries
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -35,6 +33,8 @@ color_dict_label = {'Ethyl Butyrate': color1, 'Alpha-pinene': color2, 'Alpha pin
                     '2-Heptanone' : color2, 'Methyl Acetate': color1, 'Fenchone': color3, '2,3-Butanedione': color4,
                     'Methyl Butyrate': color1, 
                     '90': color1, '60': color2, '0': color3}
+
+results_path = r'C:\Users\tiffany.ona\OneDrive - Allen Institute\Documents\VR foraging\experiments\batch 4 - manipulating cost of travelling and global statistics\results'
 
 def set_clean_yaxis(ax, data, variable, annotation_top=None, n_ticks=4):
 
@@ -104,9 +104,21 @@ def exponential_func(x, a, b):
 def format_func(value, tick_number):
     return f"{value:.0f}"
 
-results_path = r'C:\Users\tiffany.ona\OneDrive - Allen Institute\Documents\VR foraging\experiments\batch 4 - manipulating cost of travelling and global statistics\results'
-
 def plot_lines(data: pd.DataFrame, ax, variable='total_rewards', group='patch_label', one_line='mouse', order=None):
+    """
+    Plots individual lines for a specified variable across groups for each unique entity in the data.
+    Args:
+        data (pd.DataFrame): The input DataFrame containing the data to plot.
+        ax (matplotlib.axes.Axes): The matplotlib axes object on which to plot.
+        variable (str, optional): The column name in `data` to use for the y-axis values. Defaults to 'total_rewards'.
+        group (str, optional): The column name in `data` to use for grouping on the x-axis. Defaults to 'patch_label'.
+        one_line (str, optional): The column name in `data` representing individual entities (e.g., 'mouse'), each of which will have a separate line. Defaults to 'mouse'.
+        order (list, optional): The order of group labels to use on the x-axis. If None, uses sorted unique values from the `group` column.
+    Notes:
+        - Each unique value in `one_line` will be plotted as a separate line.
+        - X-axis positions are mapped from group labels according to the `order` parameter.
+        - Lines are plotted with black color, partial transparency, and no markers.
+    """
     if order is None:
         order = sorted(data['patch_label'].unique())
 
@@ -121,6 +133,30 @@ def plot_lines(data: pd.DataFrame, ax, variable='total_rewards', group='patch_la
         ax.plot(x, y, marker='', linestyle='-', color='black', alpha=0.4, linewidth=1)
 
 def plot_significance(general_df: pd.DataFrame, axes, variable = 'total_rewards', group='patch_label', conditions = ['90', '60']):
+    """
+    Performs a statistical significance test between two groups in a DataFrame and annotates the result on a given matplotlib axes.
+    Parameters
+    ----------
+    general_df : pd.DataFrame
+        The DataFrame containing the data to be compared.
+    axes : matplotlib.axes.Axes
+        The axes object on which to plot the significance annotation.
+    variable : str, optional
+        The column name in `general_df` to compare between groups (default is 'total_rewards').
+    group : str, optional
+        The column name in `general_df` that defines the grouping variable (default is 'patch_label').
+    conditions : list of str, optional
+        The two group labels to compare (default is ['90', '60']).
+    Returns
+    -------
+    float
+        The y-coordinate at the top of the significance annotation.
+    Notes
+    -----
+    - Performs a paired t-test (`ttest_rel`) by default. If this fails, falls back to an independent t-test (`ttest_ind`).
+    - Annotates the axes with the significance level ('ns', '*', '**', or '***') based on the p-value.
+    - Adjusts annotation position for the 'reward_probability' variable.
+    """
         # Perform statistical test and add significance annotations
     group1 = general_df.loc[general_df[group]== conditions[0], variable]
     group2 = general_df.loc[general_df[group] == conditions[1], variable]
@@ -179,8 +215,11 @@ def summary_main_variables(general_df,
         
     general_df = general_df.loc[(general_df.patch_label != '0')]
     
-    if not experiment:
-        general_df = general_df.loc[general_df.experiment == experiment]
+    if experiment:
+        if 'experiment' in general_df.columns:
+            general_df = general_df.loc[general_df.experiment == experiment]
+        else:
+            general_df = general_df.loc[general_df.stage == experiment]
     
     axes = ax[0][0]
     variable = 'total_rewards'
@@ -269,13 +308,45 @@ def summary_main_variables(general_df,
         plt.savefig(results_path+'/summary_mouse.pdf', dpi=300, bbox_inches='tight')
     plt.show()
 
+experiments_palette = {
+    'data_collection': '#1f77b4',  # Blue
+    'control': "#bebebe",  # Red
+    'friction_high': '#6a51a3',  # Purple
+    'friction_med': '#807dba',  # Lighter Purple
+    'friction_low': '#9e9ac8',  # Lightest Purple
+    'distance_extra_short': 'crimson',  # Blue
+    'distance_short': 'pink',  # Lighter Blue
+    'distance_extra_long': '#fd8d3c',  # Yellow
+    'distance_long': '#fdae6b',  # Lighter Yellow
+    'odor_60': '#1b9e77',  # Green
+    'odor_90': '#d95f02',  # Orange
+}
+
 def across_sessions_one_plot(summary_df, variable, save=False):
+    """
+    Plots a scatterplot of a specified variable across sessions for each mouse, 
+    with points colored by experiment and styled by patch label.
+    Parameters
+    ----------
+    summary_df : pandas.DataFrame
+        DataFrame containing summary data with at least the following columns:
+        'experiment', 'patch_label', 'mouse', 'session_n', 'site_number', and the specified variable.
+    variable : str
+        The name of the column in `summary_df` to plot on the y-axis.
+    save : str or bool, optional
+        If a string (file path) is provided, saves each figure as a PDF to the specified path.
+        If False (default), figures are not saved.
+    Returns
+    -------
+    None
+        Displays the plots for each mouse. Optionally saves the plots as PDF files.
+    """
     experiments = summary_df['experiment'].unique()
     palette = sns.color_palette("tab10", len(experiments))
-    color_dict_experiment = dict(zip(experiments, palette))
+    color_dict_experiment = dict(zip(experiments, experiments_palette))
 
     # Create a style dictionary for each odor label
-    odor_labels = summary_df['odor_label'].unique()
+    odor_labels = summary_df['patch_label'].unique()
     styles = ['o', 's', 'D', '^', 'v', '<', '>', 'p', '*', 'h']
     style_dict_odor_label = dict(zip(odor_labels, styles))
     
@@ -283,8 +354,8 @@ def across_sessions_one_plot(summary_df, variable, save=False):
     max_value = summary_df[variable].max()
     for i, mouse in enumerate(summary_df.mouse.unique()):
         fig = plt.figure(figsize=(20,6))
-        sns.scatterplot(summary_df.loc[(summary_df.mouse == mouse)], x='session_n', size="site_number", hue='experiment', style='odor_label', sizes=(30, 500), y=variable, 
-                        palette=color_dict_experiment,  alpha=0.7,
+        sns.scatterplot(summary_df.loc[(summary_df.mouse == mouse)], x='session_n', size="site_number", hue='experiment', style='patch_label', sizes=(30, 500), y=variable, 
+                        palette=experiments_palette,  alpha=0.7,
                         markers=style_dict_odor_label)
 
         plt.xlabel('')
@@ -298,6 +369,28 @@ def across_sessions_one_plot(summary_df, variable, save=False):
             fig.savefig(save, format='pdf')
             
 def across_sessions_multi_plot(summary_df, variable, condition: str = 'None', save=False):
+    """
+    Plots data across multiple experimental sessions, visualizing a specified variable for each unique experiment.
+    Parameters
+    ----------
+    summary_df : pandas.DataFrame
+        DataFrame containing summary data for plotting. Must include columns: 'experiment', 'within_session_n', 'site_number', 'odor_label', and optionally 'mouse'.
+    variable : str
+        The name of the column in `summary_df` to plot on the y-axis.
+    condition : str, optional
+        Condition for plot customization. If set to 'mouse', the plot will be titled with the mouse identifier. Default is 'None'.
+    save : bool or str, optional
+        If False, the plot is not saved. If a string (filepath), the plot is saved as a PDF to the specified path. Default is False.
+    Returns
+    -------
+    None
+        Displays the generated plots and optionally saves them to a file.
+    Notes
+    -----
+    - Uses seaborn for scatter and line plots.
+    - Each subplot corresponds to a unique experiment in `summary_df`.
+    - The function expects a global variable `color_dict_label` for color mapping.
+    """
 
     fig = plt.figure(figsize=(18,10))
     
@@ -320,6 +413,31 @@ def across_sessions_multi_plot(summary_df, variable, condition: str = 'None', sa
     plt.show()
 
 def plot_velocity_across_sessions(cum_velocity, save=False, xlim = [-1, 2]):
+    """
+    Plots velocity traces across multiple experimental sessions.
+    This function generates a multi-panel figure showing the velocity (speed) of subjects over time,
+    aligned to the start of inter-patch intervals, for different experiments and sessions. The first subplot
+    shows all experiments together, while subsequent subplots show individual experiments with sessions colored.
+    Parameters
+    ----------
+    cum_velocity : pandas.DataFrame
+        DataFrame containing velocity data with at least the following columns:
+        'times' (time from inter-patch start), 'speed' (velocity), 'experiment' (experiment label),
+        'cropped' (boolean for valid data), and 'within_session_n' (session number).
+    save : bool, optional
+        If True, saves the generated figure using the provided save function. Default is False.
+    xlim : list of float, optional
+        Limits for the x-axis (time), specified as [xmin, xmax]. Default is [-1, 2].
+    Returns
+    -------
+    None
+        The function displays the plot and optionally saves it if `save` is True.
+    Notes
+    -----
+    - Requires matplotlib.pyplot as plt and seaborn as sns to be imported.
+    - Assumes `color1` is defined in the global scope for shading.
+    - The function uses hardcoded color palettes for up to 8 experiments.
+    """
     fig = plt.figure(figsize=(12,22))
 
     fig.add_subplot(5,2,1)
@@ -352,6 +470,23 @@ def plot_velocity_across_sessions(cum_velocity, save=False, xlim = [-1, 2]):
         save.savefig(fig)
         
 def torque_plots(cum_torque, limits: list = [1500, 2400], save= False):
+    """
+    Plots torque data aligned to event onset and offset for friction experiments.
+    Parameters
+    ----------
+    cum_torque : pandas.DataFrame
+        DataFrame containing torque measurements with columns including 'align', 'times', 'Torque', and 'experiment'.
+        The 'align' column should specify alignment type ('onset' or 'offset').
+    limits : list, optional
+        Y-axis limits for the torque plots, specified as [min, max]. Default is [1500, 2400].
+    save : bool, optional
+        If True, saves the generated figure using the provided save function. Default is False.
+    Notes
+    -----
+    - The function creates two subplots: one aligned to event onset and one to event offset.
+    - Shaded regions are added to indicate different experimental phases.
+    - Requires seaborn (sns) and matplotlib.pyplot (plt) to be imported, as well as color1 to be defined in the scope.
+    """
     fig = plt.figure(figsize=(12,4))
     fig.add_subplot(121)
 
@@ -376,3 +511,110 @@ def torque_plots(cum_torque, limits: list = [1500, 2400], save= False):
     plt.show()
     if save:
         save.savefig(fig)
+        
+        
+def plot_experiments_comparison(ax, summary_df, variable: str = 'reward_probability', 
+                                experiments= ['control', 'friction_low', 'friction_med', 'friction_high', 'distance_short', 'distance_long', 'distance_extra_short', 'distance_extra_long']):
+        """
+        Plots a comparison of experimental conditions using boxplots and swarmplots for a specified variable.
+        Parameters
+        ----------
+        ax : matplotlib.axes.Axes
+            The axes object on which to plot.
+        summary_df : pandas.DataFrame
+            DataFrame containing experimental data. Must include 'experiment', 'mouse', 'patch_label', and the specified variable columns.
+        variable : str, optional
+            The variable to plot on the y-axis (default is 'reward_probability').
+        experiments : list of str, optional
+            List of experiment names to include and order on the x-axis (default is 
+            ['control', 'friction_low', 'friction_med', 'friction_high', 'distance_short', 'distance_long', 'distance_extra_short', 'distance_extra_long']).
+        Notes
+        -----
+        - Excludes rows where 'patch_label' is 'Amyl Acetate'.
+        - Aggregates data by experiment and mouse, taking the mean of the specified variable.
+        - Uses seaborn to plot boxplots and swarmplots for each experiment.
+        - Customizes x-tick labels, y-axis limits, and labels based on the variable.
+        - Draws a horizontal dashed line at the mean value of the control group.
+        - Removes plot spines and applies tight layout for better appearance.
+        """
+        labels_dict = {'control': 'Control', 'friction_low': 'Friction low', 'friction_med': 'Friction med', 'friction_high': 'Friction high', 'distance_short': 'Dis. short', 'distance_long': 'Dis. long', 'distance_extra_short': 'Ext. short', 'distance_extra_long': 'Ext. long', 'odor_60': 'Odor 60', 'odor_90': 'Odor 90'}
+
+        
+        summary_df = summary_df.loc[summary_df.patch_label != 'Amyl Acetate']
+        summary_df = summary_df.groupby(['experiment', 'mouse']).agg({variable:'mean'}).reset_index()
+        # Adjust the linewidth or dodge parameter based on experiment_width
+        sns.boxplot(x='experiment', y=variable,
+                    data=summary_df, palette=experiments_palette,
+                    showfliers=False, ax=ax,  order= experiments)  # Example adjustment
+        sns.swarmplot(x='experiment', y=variable, legend=False, color='black', data=summary_df, dodge=True, ax=ax, order=experiments)
+        
+        for experiment in experiments:
+            labels_plot = [labels_dict[experiment] for experiment in experiments]
+            
+        ax.set_xticks(np.arange(len(experiments)), labels_plot, rotation = 45, ha='right')
+        ax.set_ylabel(variable)
+        # Additional plot adjustments
+        ax.set_title('All')
+        ax.hlines(summary_df.loc[(summary_df.experiment == 'control')][variable].mean(), -1, len(experiments), linestyles='dashed', alpha=0.5, color='grey')
+        ax.set_xlabel('')
+        if variable == 'reward_probability':
+            ax.set_yticks([0, 0.2, 0.4, 0.6])
+            ax.set_ylim(0, 0.6)
+            ax.set_ylabel('P(reward) when leaving')
+        if variable == 'stops':
+            ax.set_ylim(0, 20)
+            ax.set_ylabel('Stops')
+        sns.despine()
+        plt.tight_layout()
+        
+
+def plot_experiments_comparison_with_odors(ax, summary_df, variable, 
+                                           experiments= ['control', 'friction_low', 'friction_med', 'friction_high', 'distance_short', 'distance_long', 'distance_extra_short', 'distance_extra_long'], 
+                                           mean_line = 'control'):
+    """
+    Plots boxplots and swarmplots for a given variable across different experimental conditions and odor patch labels.
+    Parameters
+    ----------
+    ax : matplotlib.axes.Axes
+        The matplotlib axes object on which to plot.
+    summary_df : pandas.DataFrame
+        DataFrame containing summary statistics for each trial, including columns 'experiment', 'patch_label', and the variable to plot.
+    variable : str
+        The name of the column in `summary_df` to plot on the y-axis.
+    experiments : list of str, optional
+        List of experiment names to include and their order on the x-axis. Defaults to a predefined list of experiment conditions.
+    Notes
+    -----
+    - Uses seaborn to plot boxplots and swarmplots, colored by 'patch_label'.
+    - Adjusts x-tick labels, y-axis limits, and labels based on the variable.
+    - Draws a horizontal dashed line at the mean value of the control experiment for reference.
+    - Assumes `color_dict_label` is defined in the global scope for color mapping.
+    - Filters out rows where 'patch_label' is '0'.
+    """
+    
+    labels_dict = {'data_collection': 'Data collection', 'control': 'Control', 'friction_low': 'Friction low', 'friction_med': 'Friction med', 'friction_high': 'Friction high', 'distance_short': 'Dis. short', 'distance_long': 'Dis. long', 'distance_extra_short': 'Ext. short', 'distance_extra_long': 'Ext. long', 'odor_60': 'Odor 60', 'odor_90': 'Odor 90'}
+    
+    
+    summary_df = summary_df.loc[summary_df.patch_label != '0']
+
+    # Adjust the linewidth or dodge parameter based on experiment_width
+    sns.boxplot(x='experiment', y=variable, hue='patch_label',
+                palette=color_dict_label, data=summary_df,
+                showfliers=False, ax=ax,  order=experiments)  # Example adjustment
+    sns.swarmplot(x='experiment', y=variable, hue='patch_label', legend=False, data=summary_df, dodge=True, palette=color_dict_label, ax=ax, order=experiments)
+    # Additional plot adjustments
+    ax.set_title('All')
+    for experiment in experiments:
+        labels_plot = [labels_dict[experiment] for experiment in experiments]
+        
+    ax.set_xticks(np.arange(len(experiments)), labels_plot, rotation = 45, ha='right')
+    ax.hlines(summary_df.loc[(summary_df.experiment == mean_line)][variable].mean(), -0.5, len(experiments)-0.5, linestyles='dashed', alpha=0.5, color='grey')
+    ax.set_xlabel('')
+    if variable == 'reward_probability':
+        ax.set_yticks([0, 0.2, 0.4, 0.6])
+        ax.set_ylim(0, 0.6)
+        ax.set_ylabel('P(reward) when leaving')
+    if variable == 'stops':
+        ax.set_ylim(0, 20)
+    sns.despine()
+    plt.tight_layout()
